07d04668dbecbf5ac3e95a8623069ace446f274e
charm0625 <cbkgomotso@gmail.com>
2026-03-22 20:23:11 +0200

    Deploy MNIST TFLite app to Streamlit Cloud

--- app.py
+++ app.py
@@ -1,27 +1,32 @@
 import streamlit as st
 import tensorflow as tf
-from tensorflow.keras.models import load_model
 import numpy as np
 from PIL import Image
 
-#Load the trained model
-model = load_model("mnist_model.keras")
+#Load the TFLite model
+interpreter = tf.lite.Interpreter(model_path="mnist_model.tflite")
+interpreter.allocate_tensors()
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
 st.title("MNIST Digit Classifier")
+st.write("Upload an image of a handwritten digit (28X28 grayscale).")
 
 #Upload image
-uploaded_file = st.file_uploader("Upload a MNIST image", type=["png", "jpg", "jpeg"])
+uploaded_file = st.file_uploader("Upload Image", type=["png", "jpg", "jpeg"])
 
 if uploaded_file:
 
-    image = Image.open(uploaded_file).convert("L")
-    image = image.resize((28,28))
-
+    image = Image.open(uploaded_file).convert("L").resize((28,28))
     img_array = np.array(image) / 255.0
-    img_array = img_array.reshape(1,28,28)
 
-    prediction = model.predict(img_array)
-    digit = np.argmax(prediction)
+    img_array = img_array.reshape(1,28,28,1).astype(np.float32)
 
-    st.image(image, caption="Uploaded Image")
-    st.write("Prediction:", digit)
+    #Make prediction
+    interpreter.set_tensor(input_details[0]['index'], img_array)
+    interpreter.invoke()
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    prediction = np.argmax(output_data)
+
+    st.image(image, caption="Uploaded Image", width=150)
+    st.success(f"Predicted Digit: {prediction}")
